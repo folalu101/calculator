55fe75c0285d8c1b5bb014b149bd343d72739dd2
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-20 19:42:42 +0100

    add more jquery code to js file and ad id to button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
     $("#form-holder").submit(function(event){
+        const num1 = $("#num-1").val();
+        const num2 = $("#num-2").val();
+
+        $("#num-1").text(num1);
+        $("#num-2").text(num2);
+
+        $("#result").show();
 
 
         event.preventDefault();
     });
 
     // mathimatical Functions
-
+   
     // Addition
     function add(num1, num2){
         return num1 + num2;
@@ -22,6 +29,10 @@ $(document).ready(function(){
 
     function multiply(num1, num2){
         return num1 * num2;
+    }
+
+    function modulus(num1, num2){
+        return num1 % num2;
     }
 
 
