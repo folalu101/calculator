174e5b238ae3b2bad11a2e6eb2f81e75bf4e3fcd
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-20 19:27:28 +0100

    add action functions in jquery on js file

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
-    
+    $("#form-holder").submit(function(event){
+
+
+        event.preventDefault();
+    });
+
+    // mathimatical Functions
+
+    // Addition
+    function add(num1, num2){
+        return num1 + num2;
+    }
+
+    function divide(num1, num2){
+        return num1 / num2;
+    }
+
+    function subtract(num1, num2){
+        return num1 - num2;
+    }
+
+    function multiply(num1, num2){
+        return num1 * num2;
+    }
+
+
 
 });
