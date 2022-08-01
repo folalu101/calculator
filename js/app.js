6993d580e52f1ddb7749de2e734121bdb17a3476
A.Adefolalu B <adefolalucode101@gmail.com>
2022-08-01 22:49:54 +0100

    add styling to result id on index page

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    // mathimatical Functions
+//     // mathimatical Functions
    
     
-    function add(num1, num2){
-        return num1 + num2;
-    }
+//     function add(num1, num2){
+//         return num1 + num2;
+//     }
        
     
-    function divide(num1, num2){
-        return num1 / num2;
-    }
+//     function divide(num1, num2){
+//         return num1 / num2;
+//     }
        
     
-    function subtract(num1, num2){
-        return num1 - num2;
-    }
+//     function subtract(num1, num2){
+//         return num1 - num2;
+//     }
        
     
-    function multiply(num1, num2){
-        return num1 * num2;
-    }
+//     function multiply(num1, num2){
+//         return num1 * num2;
+//     }
        
     
-    function modulus(num1, num2){
-        return num1 % num2;
-    }
+//     function modulus(num1, num2){
+//         return num1 % num2;
+//     }
 
-    $("#form-holder").submit(function(event){
-        const num1 = $("#num-1").val();
-        const num2 = $("#num-2").val();
-        // const result = add(num1, num2);
+//     $("#form-holder").submit(function(event){
+//         const num1 = $("#num-1").val();
+//         const num2 = $("#num-2").val();
+//         // const result = add(num1, num2);
         
-        $("#num-1").text(num1);
-        $("#num-2").text(num2);
+//         $("#num-1").text(num1);
+//         $("#num-2").text(num2);
         
        
 
-        
-
-        $("#answer").show();
-
-
-        event.preventDefault();
-    });
-
-    $("#addition").click(function(event){
-        const num1 = $("#num-1").val();
-        const num2 = $("#num-2").val();
-        const result = add(num1, num2);
-
-        $("#num-1").text(num1);
-        $("#num-2").text(num2);
-        $("#answer").text(result);
-       
-
-
-        $("#aswer").show();
-
-
-        event.preventDefault();
-    });
-
-    
         
 
+//         $("#answer").show();
 
 
-});
+//         event.preventDefault();
+//     });
+
+//     $("#addition").click(function(event){
+//         const num1 = $("#num-1").val();
+//         const num2 = $("#num-2").val();
+//         const result = add(num1, num2);
+
+//         $("#num-1").text(num1);
+//         $("#num-2").text(num2);
+//         $("#answer").text(result);
+       
+
+
+//         $("#aswer").show();
+
+
+//         event.preventDefault();
+//     });
+
+    
+        
+
+
+
+// });
