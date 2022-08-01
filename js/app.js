251cd3f7dbcc12d830b925912f9958cf1edbe815
A.Adefolalu B <adefolalucode101@gmail.com>
2022-08-01 22:28:05 +0100

    add button and class name and attribute

--- js/app.js
+++ js/app.js
@@ -1,39 +1,69 @@
 $(document).ready(function(){
+
+    // mathimatical Functions
+   
+    
+    function add(num1, num2){
+        return num1 + num2;
+    }
+       
+    
+    function divide(num1, num2){
+        return num1 / num2;
+    }
+       
+    
+    function subtract(num1, num2){
+        return num1 - num2;
+    }
+       
+    
+    function multiply(num1, num2){
+        return num1 * num2;
+    }
+       
+    
+    function modulus(num1, num2){
+        return num1 % num2;
+    }
+
     $("#form-holder").submit(function(event){
         const num1 = $("#num-1").val();
         const num2 = $("#num-2").val();
-
+        // const result = add(num1, num2);
+        
         $("#num-1").text(num1);
         $("#num-2").text(num2);
+        
+       
 
-        $("#result").show();
+        
+
+        $("#answer").show();
 
 
         event.preventDefault();
     });
 
-    // mathimatical Functions
-   
-    // Addition
-    function add(num1, num2){
-        return num1 + num2;
-    }
+    $("#addition").click(function(event){
+        const num1 = $("#num-1").val();
+        const num2 = $("#num-2").val();
+        const result = add(num1, num2);
 
-    function divide(num1, num2){
-        return num1 / num2;
-    }
+        $("#num-1").text(num1);
+        $("#num-2").text(num2);
+        $("#answer").text(result);
+       
 
-    function subtract(num1, num2){
-        return num1 - num2;
-    }
 
-    function multiply(num1, num2){
-        return num1 * num2;
-    }
+        $("#aswer").show();
 
-    function modulus(num1, num2){
-        return num1 % num2;
-    }
+
+        event.preventDefault();
+    });
+
+    
+        
 
 
 
